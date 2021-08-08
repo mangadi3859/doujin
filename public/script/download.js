39a1b5b63f12eca2df705585535e40c08157ed3c
ISLA <65892564+mangadi3859@users.noreply.github.com>
2021-08-08 08:39:13 +0800

    Delete

--- public/script/download.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const button = document.querySelector("[download-btn]");
-// button.addEventListener("click", () => {
-//     axios.post("/download/isla", {
-//         id: doujinId,
-//     });
-// });
